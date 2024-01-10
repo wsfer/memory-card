033f9af16b07a669e4f8a4e03b60f1115fdba52a
wsfer <93054415+wsfer@users.noreply.github.com>
2024-01-10 11:36:07 -0300

    feat: create title/logo component

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,12 @@
+import './Title.css';
+
+export function Title() {
+  return (
+    <span className="title">
+      P<span className="title__overlap-letter">o</span>k
+      <span className="title__overlap-letter">é</span>m
+      <span className="title__overlap-letter">e</span>m
+      <span className="title__overlap-letter">o</span>
+    </span>
+  );
+}
