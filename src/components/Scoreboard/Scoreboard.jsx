d1e71dac7e86819f14e78e3910b0d4c68da4d4df
wsfer <93054415+wsfer@users.noreply.github.com>
2024-01-10 15:56:21 -0300

    feat: add style for scoreboard component

--- src/components/Scoreboard/Scoreboard.jsx
+++ src/components/Scoreboard/Scoreboard.jsx
@@ -1,10 +1,17 @@
+import { Container } from '../Container/Container';
+import './Scoreboard.css';
+
 export function Scoreboard({ currentScore, bestScore }) {
   return (
     <section aria-label="Scoreboard" className="scoreboard">
-      {currentScore && (
-        <p className="scoreboard__text">Score: {currentScore}</p>
-      )}
-      <p className="scoreboard__text">Best Score: {bestScore || 0}</p>
+      <Container>
+        <div className="scoreboard__row">
+          {currentScore && (
+            <p className="scoreboard__text">Score: {currentScore}</p>
+          )}
+          <p className="scoreboard__text">Best Score: {bestScore || 0}</p>
+        </div>
+      </Container>
     </section>
   );
 }
