189e28ec98340ddefc61fd45eb0d559c4309f037
wsfer <93054415+wsfer@users.noreply.github.com>
2024-01-10 15:58:46 -0300

    feat: add scoreboard on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { useState } from 'react';
+import { useBestScore } from './hooks/useBestScore';
 import { Header } from './components/Header/Header';
+import { Scoreboard } from './components/Scoreboard/Scoreboard';
 import './App.css';
 
 function App() {
-  return <Header />;
+  const [score, setScore] = useState(null);
+  const bestScore = useBestScore(score);
+
+  return (
+    <>
+      <Header />
+      <Scoreboard currentScore={score} bestScore={bestScore} />
+    </>
+  );
 }
 
 export default App;
