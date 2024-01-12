f881a815f6b6a74bde0e821880da5c87b36c8633
wsfer <93054415+wsfer@users.noreply.github.com>
2024-01-12 11:32:12 -0300

    feat: add startscreen component on page

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,26 @@ import { useState } from 'react';
 import { useBestScore } from './hooks/useBestScore';
 import { Header } from './components/Header/Header';
 import { Scoreboard } from './components/Scoreboard/Scoreboard';
+import { StartScreen } from './components/StartScreen/StartScreen';
 import { Footer } from './components/Footer/Footer';
 import './App.css';
 
 function App() {
   const [score, setScore] = useState(null);
+  const [difficulty, setDifficulty] = useState('normal');
   const bestScore = useBestScore(score);
+
+  const handleStartGame = () => {};
 
   return (
     <>
       <Header />
       <Scoreboard currentScore={score} bestScore={bestScore} />
+      <StartScreen
+        difficulty={difficulty}
+        setDifficulty={setDifficulty}
+        startGame={handleStartGame}
+      />
       <Footer />
     </>
   );
