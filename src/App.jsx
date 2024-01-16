05ff7681cbb5cda0ffe60d0a9fa75a93f2d4ddc7
wsfer <93054415+wsfer@users.noreply.github.com>
2024-01-16 11:56:03 -0300

    feat: add game screen component on the page

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,44 @@ import { useBestScore } from './hooks/useBestScore';
 import { Header } from './components/Header/Header';
 import { Scoreboard } from './components/Scoreboard/Scoreboard';
 import { StartScreen } from './components/StartScreen/StartScreen';
+import { GameScreen } from './components/GameScreen/GameScreen';
 import { Footer } from './components/Footer/Footer';
 import './App.css';
 
 function App() {
   const [score, setScore] = useState(null);
   const [difficulty, setDifficulty] = useState('normal');
+  const [isGameOver, setIsGameOver] = useState(true);
   const bestScore = useBestScore(score);
 
-  const handleStartGame = () => {};
+  const handleStartGame = () => {
+    setIsGameOver(false);
+    setScore(0);
+  };
+
+  const handleGameOver = () => {
+    setIsGameOver(true);
+    setScore(null);
+  };
 
   return (
     <>
       <Header />
       <Scoreboard currentScore={score} bestScore={bestScore} />
-      <StartScreen
-        difficulty={difficulty}
-        setDifficulty={setDifficulty}
-        startGame={handleStartGame}
-      />
+      {isGameOver ? (
+        <StartScreen
+          difficulty={difficulty}
+          setDifficulty={setDifficulty}
+          startGame={handleStartGame}
+        />
+      ) : (
+        <GameScreen
+          difficulty={difficulty}
+          gameOver={handleGameOver}
+          score={score}
+          setScore={setScore}
+        />
+      )}
       <Footer />
     </>
   );
