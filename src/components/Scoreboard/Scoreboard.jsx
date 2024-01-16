981ac630edf503e62a4b4ddf1b39ab1a8a19b764
wsfer <93054415+wsfer@users.noreply.github.com>
2024-01-16 15:14:22 -0300

    feat: add an animated element on scoreboard to display gained score on each round

--- src/components/Scoreboard/Scoreboard.jsx
+++ src/components/Scoreboard/Scoreboard.jsx
@@ -1,13 +1,31 @@
+import { useEffect, useState } from 'react';
 import { Container } from '../Container/Container';
 import './Scoreboard.css';
 
 export function Scoreboard({ currentScore, bestScore }) {
+  const [oldScore, setOldScore] = useState(0);
+
+  // Used on "gained score" element which remounts with a different key value to trigger the fadeout animation
+  const gainedScore = currentScore - oldScore;
+
+  // Set oldScore to old currentScore
+  useEffect(() => {
+    return () => setOldScore(currentScore);
+  }, [currentScore]);
+
   return (
     <section aria-label="Scoreboard" className="scoreboard">
       <Container>
         <div className="scoreboard__row">
           {currentScore !== null && (
-            <p className="scoreboard__text">Score: {currentScore}</p>
+            <p className="scoreboard__text">
+              Score: {currentScore}
+              {gainedScore > 0 && (
+                <span key={oldScore} className="scoreboard__gained-score">
+                  {'+' + gainedScore}
+                </span>
+              )}
+            </p>
           )}
           <p className="scoreboard__text">Best Score: {bestScore || 0}</p>
         </div>
