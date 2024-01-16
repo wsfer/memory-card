6e9ce7dd99a999043e349701084bf431d3260b4a
wsfer <93054415+wsfer@users.noreply.github.com>
2024-01-16 16:20:54 -0300

    feat: update gamescreen component to include gameover box

--- src/components/GameScreen/GameScreen.jsx
+++ src/components/GameScreen/GameScreen.jsx
@@ -6,14 +6,16 @@ import { Loading } from '../Loading/Loading';
 import { Container } from '../Container/Container';
 import { GameStatus } from './GameStatus';
 import { CardList } from './CardList';
+import { GameOverBox } from './GameOverBox';
 import './GameScreen.css';
 
 // This get pokemons from gen I, II, III and IV
 const API_ENDPOINT = 'https://pokeapi.co/api/v2/pokemon/?limit=493';
-const MAX_LEVEL = 7;
 
 export function GameScreen({ difficulty, gameOver, score, setScore }) {
   const { data, loading, error } = useFetch(API_ENDPOINT);
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [win, setWin] = useState(false);
   const [level, setLevel] = useState(1);
   const [pokemons, selectedPokemons, selectPokemon] = usePokemons(
     data,
@@ -27,17 +29,22 @@ export function GameScreen({ difficulty, gameOver, score, setScore }) {
     const pokemon = shuffledPokemons[index];
     const gainedScore = 100 + (level - 1) * 50;
     const { isGameOver, totalCards, clickedCards } = selectPokemon(pokemon);
+    const updateLevel = totalCards === clickedCards;
+    const isLastLevel = updateLevel && level === 7;
 
-    if (isGameOver || (totalCards === clickedCards && level === MAX_LEVEL)) {
-      gameOver();
-      return;
-    }
-
-    if (totalCards === clickedCards) {
+    if (isLastLevel) {
+      setScore(score + gainedScore);
+      setIsGameOver(true);
+      setWin(true);
+    } else if (isGameOver) {
+      setIsGameOver(true);
+      setWin(false);
+    } else if (updateLevel) {
+      setScore(score + gainedScore);
       setLevel(level + 1);
+    } else {
+      setScore(score + gainedScore);
     }
-
-    setScore(score + gainedScore);
   };
 
   return (
@@ -45,15 +52,20 @@ export function GameScreen({ difficulty, gameOver, score, setScore }) {
       {loading ? (
         <Loading message={error} />
       ) : (
-        <Container>
-          <GameStatus
-            difficulty={difficulty}
-            level={level}
-            clickedCards={selectedPokemons.length}
-            totalCards={pokemons.length}
-          />
-          <CardList pokemons={shuffledPokemons} playTurn={playTurn} />
-        </Container>
+        <>
+          <Container>
+            <GameStatus
+              difficulty={difficulty}
+              level={level}
+              clickedCards={selectedPokemons.length}
+              totalCards={pokemons.length}
+            />
+            <CardList pokemons={shuffledPokemons} playTurn={playTurn} />
+          </Container>
+          {isGameOver && (
+            <GameOverBox handleGameOver={gameOver} score={score} win={win} />
+          )}
+        </>
       )}
     </main>
   );
