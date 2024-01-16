ff8b128579abf2f0f09b011c95f25c7687852b82
wsfer <93054415+wsfer@users.noreply.github.com>
2024-01-15 23:54:29 -0300

    feat: create custom hook to keep track of current level pokemons and selected pokemons

---
+++ src/hooks/usePokemons.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { getCardNumber } from '../utils/getCardNumber';
+import { getRandomPokemons } from '../utils/getRandomPokemons';
+
+/**
+ *  - returns a new list of random pokemons each time level changes
+ *  - keep track of selected pokemons
+ *  - returns information on game status each time a pokemon is selected
+ * */
+export function usePokemons(data, level, difficulty) {
+  const [pokemons, setPokemons] = useState([]);
+  const [selectedPokemons, setSelectedPokemons] = useState([]);
+
+  const selectPokemon = (pokemon) => {
+    const totalCards = getCardNumber(difficulty, level);
+    const clickedCards = selectedPokemons.length;
+
+    if (selectedPokemons.includes(pokemon)) {
+      return { isGameOver: true, totalCards, clickedCards };
+    } else {
+      setSelectedPokemons([...selectedPokemons, pokemon]);
+      return { isGameOver: false, totalCards, clickedCards: clickedCards + 1 };
+    }
+  };
+
+  useEffect(() => {
+    if (data) {
+      const quantity = getCardNumber(difficulty, level);
+      const randomPokemons = getRandomPokemons(data.results, quantity);
+      setPokemons(randomPokemons);
+
+      return () => {
+        setSelectedPokemons([]);
+      };
+    }
+  }, [data, level, difficulty]);
+
+  return [pokemons, selectedPokemons, selectPokemon];
+}
