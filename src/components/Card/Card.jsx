9b23895c2aefbccc938adc400850f7a08cbaa2c8
wsfer <93054415+wsfer@users.noreply.github.com>
2024-01-16 11:49:30 -0300

    feat: create pokemon card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,34 @@
+import { useFetch } from '../../hooks/useFetch';
+import { captalizeString } from '../../utils/capitalizeString';
+import { Loading } from '../Loading/Loading';
+import cardback from '../../assets/cardback.jpg';
+import './Card.css';
+
+export function Card({ url, handleClick }) {
+  const { data, loading, error } = useFetch(url);
+
+  return (
+    <button
+      className={loading ? 'card card--flipped-back' : 'card'}
+      onClick={handleClick}
+    >
+      <div className="card__inner">
+        {loading ? (
+          <Loading message={error} />
+        ) : (
+          <div className="card__front">
+            <h3 className="card__front-title">{captalizeString(data.name)}</h3>
+            <img
+              className="card__front-image"
+              src={data.sprites.front_default}
+              alt={data.name}
+            />
+          </div>
+        )}
+        <div aria-hidden="true" className="card__back">
+          <img src={cardback} alt="cardback" />
+        </div>
+      </div>
+    </button>
+  );
+}
